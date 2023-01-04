b19dc42df54cdafb00d3c18fe336930ba3da8308
zyx9104 <zyx9104@gmail.com>
2023-01-04 13:09:42 +0800

    支持强制字段检查

--- main.go
+++ main.go
@@ -27,8 +27,9 @@ const (
 )
 
 var (
-	typeNames  = flag.String("type", "", "comma-separated list of type names; must be set")
+	typeNames  = flag.String("type", "Config", "comma-separated list of type names; must be set")
 	output     = ""
+	dir        = "."
 	getterTmpl = "getter"
 	setterTmpl = "setter"
 	tmpl       = map[string]string{
@@ -42,6 +43,9 @@ var (
 	return {{.Receiver}}.{{.Field}}
 }
 `,
+	}
+	typeMap = map[string]bool{
+		"Config": true,
 	}
 )
 
@@ -68,14 +72,16 @@ func main() {
 	}
 
 	types := strings.Split(*typeNames, ",")
-
+	for _, typeName := range types {
+		typeMap[typeName] = true
+	}
 	// We accept either one directory or a list of files. Which do we have?
 	args := flag.Args()
 	if len(args) == 0 {
 		// Default: process whole package in current directory.
 		args = []string{"."}
 	}
-
+	dir = args[0]
 	// Parse the package once.
 	// var dir string
 	g := Generator{
@@ -93,7 +99,7 @@ func main() {
 		if output == "" {
 			output = fmt.Sprintf("%s_lina.go", types[i])
 		}
-		outputName := filepath.Join(".", strings.ToLower(output))
+		outputName := filepath.Join(dir, strings.ToLower(output))
 		buf := g.buf[typeName]
 		var src = (buf).Bytes()
 
@@ -153,6 +159,9 @@ func (g *Generator) parsePackage(patterns []string) {
 	if len(pkgs) != 1 {
 		log.Fatalf("error: %d packages found", len(pkgs))
 	}
+	if pkgs[0].Name != "config" {
+		log.Fatalf("package name must be %q, cur package %q", "config", pkgs[0].Name)
+	}
 	g.addPackage(pkgs[0])
 }
 
@@ -173,72 +182,93 @@ func (g *Generator) addPackage(pkg *packages.Package) {
 	}
 }
 
+var structInfos []map[string][]StructFieldInfo
+
+func checkExported(string, []StructFieldInfo) {
+
+}
+
 // generate produces the String method for the named type.
 func (g *Generator) generate(typeName string) {
 	for _, file := range g.pkg.files {
 		// Set the state for this run of the walker.
 		file.typeName = typeName
-		//ast.Print(file.fileSet, file.file)
+		// ast.Print(file.fileSet, file.file)
 		if file.file != nil {
-
 			structInfo, err := ParseStruct(file.file, file.fileSet, TagName)
 			if err != nil {
 				log.Panic(err)
 			}
+			structInfos = append(structInfos, structInfo)
+		}
+	}
 
-			for stName, info := range structInfo {
-				if stName != typeName {
-					continue
-				}
-				g.Printf(stName, "package %s\n", g.pkg.name)
-				g.Printf(stName, "\n")
-
-				for _, field := range info {
-					for _, access := range field.Access {
-						switch access {
-						case SetTag:
-							if SETTER {
-								g.Printf(stName, "%s\n", genSetter(stName, field.Name, field.Type))
-							}
-						case GetTag:
-							if GETTER {
-								g.Printf(stName, "%s\n", genGetter(stName, field.Name, field.Type))
-							}
-
-						case SkipTag:
-							continue
-						default:
-							log.Fatalf("unknow tag %q in field %q struct %q", access, field.Name, stName)
-						}
-					}
-
-				}
+	for _, structInfo := range structInfos {
+		for stName, info := range structInfo {
+			if stName != typeName {
+				checkExported(stName, info)
+				continue
 			}
+			g.Printf(stName, "package %s\n", g.pkg.name)
+			g.Printf(stName, "\n")
 
+			for _, field := range info {
+				for _, access := range field.Tags {
+					switch access {
+					case SetTag:
+						g.Printf(stName, "%s\n", genSetter(stName, field.Name, field.Type))
+					case GetTag:
+						g.Printf(stName, "%s\n", genGetter(stName, field.Name, field.Type))
+					case SkipTag:
+						continue
+					default:
+						log.Fatalf("unknow tag %q in field %q struct %q", access, field.Name, stName)
+					}
+				}
+
+			}
 		}
 	}
 
 }
 
 type StructFieldInfo struct {
-	Name   string
-	Type   string
-	Access []string
+	Name string
+	Type string
+	Tags []string
 }
 type StructFieldInfoArr = []StructFieldInfo
+
+var embed bool
 
 func ParseStruct(file *ast.File, fileSet *token.FileSet, tagName string) (structMap map[string]StructFieldInfoArr, err error) {
 	structMap = make(map[string]StructFieldInfoArr)
 
-	collectStructs := func(x ast.Node) bool {
-		ts, ok := x.(*ast.TypeSpec)
-		if !ok || ts.Type == nil {
-			return true
+	var collectStructs func(ast.Node) bool
+	collectStructs = func(x ast.Node) bool {
+		var structName string
+		var ts *ast.TypeSpec
+		var ok bool
+		var s *ast.StructType
+		if !embed {
+			ts, ok = x.(*ast.TypeSpec)
+			if !ok || ts.Type == nil {
+				return true
+			}
+			structName = ts.Name.Name
 		}
 
-		structName := ts.Name.Name
-
-		s, ok := ts.Type.(*ast.StructType)
+		if embed {
+			s, ok = x.(*ast.StructType)
+			var buff bytes.Buffer
+			err := printer.Fprint(&buff, fileSet, s)
+			if err != nil {
+				log.Fatal(err)
+			}
+			structName = buff.String()
+		} else {
+			s, ok = ts.Type.(*ast.StructType)
+		}
 		if !ok {
 			return true
 		}
@@ -246,17 +276,38 @@ func ParseStruct(file *ast.File, fileSet *token.FileSet, tagName string) (struct
 
 		for _, field := range s.Fields.List {
 			if len(field.Names) == 0 {
-				continue
+				if t, ok := field.Type.(*ast.SelectorExpr); ok && t.Sel.Name == "RWMutex" {
+					continue
+				} else {
+					log.Fatalf("only sync.RWMutex can be embed")
+				}
 			}
+
 			name := field.Names[0].Name
 			info := StructFieldInfo{Name: name}
+
+			ok := typeMap[structName]
+			if ok && token.IsExported(info.Name) {
+				log.Fatalf("field %q in %s musts be UnExported", info.Name, structName)
+			}
+			if !ok && !token.IsExported(info.Name) {
+				log.Fatalf("field %q in %s musts be Exported", info.Name, structName)
+			}
+
 			var typeNameBuf bytes.Buffer
+
 			err := printer.Fprint(&typeNameBuf, fileSet, field.Type)
 			if err != nil {
 				log.Println("get type failed:", err)
 				return true
 			}
 			info.Type = typeNameBuf.String()
+			if t, ok := field.Type.(*ast.StructType); ok {
+				embed = true
+				collectStructs(t)
+				embed = false
+			}
+
 			if field.Tag != nil {
 				tag := field.Tag.Value
 				tag = strings.Trim(tag, "`")
@@ -266,15 +317,13 @@ func ParseStruct(file *ast.File, fileSet *token.FileSet, tagName string) (struct
 				}
 				access, err := tags.Get(tagName)
 				if err != nil {
-					info.Access = []string{GetTag, SetTag}
+					info.Tags = []string{GetTag, SetTag}
 				} else {
 					access.Options = append(access.Options, access.Name)
-					info.Access = access.Options
+					info.Tags = access.Options
 				}
-			} else if name == "mutex" {
-				info.Access = []string{LockTag}
 			} else if !token.IsExported(name) {
-				info.Access = []string{GetTag, SetTag}
+				info.Tags = []string{GetTag, SetTag}
 			}
 			fileInfos = append(fileInfos, info)
 		}
@@ -299,7 +348,7 @@ func genFunc(funcName, structName, fieldName, typeName, lockName string) string 
 	t = template.Must(t.Parse(tmpl[funcName]))
 	res := bytes.NewBufferString("")
 	upperName := fmt.Sprintf("%s%s", strings.ToUpper(fieldName[0:1]), fieldName[1:])
-	t.Execute(res, map[string]string{
+	err := t.Execute(res, map[string]string{
 		"Receiver":   strings.ToLower(structName[0:1]),
 		"Struct":     structName,
 		"Field":      fieldName,
@@ -307,5 +356,8 @@ func genFunc(funcName, structName, fieldName, typeName, lockName string) string 
 		"UpperField": upperName,
 		"Lock":       lockName,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	return res.String()
 }
